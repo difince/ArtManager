96e9287a97e590f2ec315bb894956e1feac305e8
Angel Raev <audulin@gmail.com>
2016-01-05 16:27:48 +0200

    connect project to Parse and create objects

--- ArtManager/src/main/java/com/artmanager/artmanager/MainActivity.java
+++ ArtManager/src/main/java/com/artmanager/artmanager/MainActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import com.artmanager.artmanager.model.Shop;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -12,6 +13,15 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Shop saveShop = new Shop();
+        saveShop.setAddress("add");
+        saveShop.setCity("Sofia");
+        saveShop.setContactName("Didi");
+        saveShop.setDetails("det");
+        saveShop.setName("name");
+        saveShop.setTelephone("+359882545");
+        saveShop.saveInBackground();
     }
 
 
